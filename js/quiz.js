a28d1b184a3e1c426f95d8f4a0c825f321ce0b25
cisspbru <32341115+cisspbru@users.noreply.github.com>
2018-12-15 00:15:51 +0300

    ok

--- js/quiz.js
+++ js/quiz.js
@@ -19,8 +19,8 @@ function next(question,direction) {
             q_title.innerHTML = "Куда требуется видеонаблюдение?";
             q_progress_bar.innerHTML = '<div class=\"row2\"><div class=\"w3-light-grey\"><div class=\"w3-container w3-green w3-center\" style=\"width:10%\"></div></div></div><div class=\"textRow2\">1-ый вопрос из 9</div>';
             q_main_content.innerHTML = q_main_content_generator();
-            q_button.innerHTML = "<div class=\"button-q4\">  <button onclick=\"next('question2','forward'); \" class=\"input-btn\">Далее</button></div>";
-            q_additional_content.innerHTML = "В первую очередь важен тип объекта, поэтому для начала выберите куда нужно уствновить систему. Это повысит точность расчета."
+            q_button.innerHTML = "<div class=\"row5\" ><button onclick=\"next('question2');\" class=\"input-btn\">Далее</button></div>";
+            q_additional_content.innerHTML = "<div class=\"quiz1right\" id=\"q_additional_content\"><div class=\"right_img\"><img src=\"img/q1.1.png\" alt=\"\"> </div><div class=\"right_text\"><hr>В первую очередь важен тип объекта, поэтому для начала выберите куда нужно уствновить систему. Это повысит точность расчета.</div><div class=\"right_footer\"><div class=\"rf_img\"><img src=\"img/small-logo2.png\" alt=\"\"> </div><div class=\"rf_text\">Установка систем безопасности \"под ключ\"</div></div>";
             break;
         case 'question2':
             if (direction == 'forward') { temp = document.getElementById('q1').value; }
@@ -28,13 +28,13 @@ function next(question,direction) {
             q_progress_bar.innerHTML = '<div class=\"row2\"><div class=\"w3-light-grey\"><div class=\"w3-container w3-green w3-center\" style=\"width:20%\"></div></div></div><div class=\"textRow2\">2-ой вопрос из 9</div>';
             q_main_content.innerHTML = "<div class=\"q2\"><div class=\"text-q2\">Укажите общую длину кабеля</div><input type=\"number\" id = \"q2\" value = \"" + square+"\"> <div class=\"check-q2\"><input type=\"checkbox\" id=\"chack_q2\" name=\"chack\" value=\"chack\">   Не знаю (в данном случае стоимость будет указана за метр)</div></div>";
             q_button.innerHTML = "<div class=\"button-q4\"><button onclick=\"next('question1','back'); \" class=\"input-btn\">Назад</button>   <button onclick=\"next('question3','forward'); \" class=\"input-btn\">Далее</button></div>";
-            q_additional_content.innerHTML = "Во вторую очередь важен тип объекта, поэтому для начала выберите куда нужно уствновить систему. Это повысит точность расчета."
+            q_additional_content.innerHTML = "<div class=\"quiz1right\" id=\"q_additional_content\"><div class=\"right_img\"><img src=\"img/q2.png\" alt=\"\"> </div><div class=\"right_text\"><hr>Нужно измерить расстояние от видеорегистратора до камер. На примере показано как примерно следует делать измерения.</div><div class=\"right_footer\"><div class=\"rf_img\"><img src=\"img/small-logo2.png\" alt=\"\"> </div><div class=\"rf_text\">Установка систем безопасности \"под ключ\"</div></div>";
             break;
         case 'question3':
             if (direction == 'forward') { square = document.getElementById('q2').value }
             q_title.innerHTML = "Сколько понадобится видеокамер?";            
             q_progress_bar.innerHTML = '<div class=\"row2\"><div class=\"w3-light-grey\"><div class=\"w3-container w3-green w3-center\" style=\"width:30%\"></div></div></div><div class=\"textRow2\">3-ий вопрос из 9</div>';
-            q_main_content.innerHTML = "<div class=\"q3\"><select id=\"question3\"><option value=\"choice\">-Выбрать-</option><option value=\"nol\">Не знаю, жду рекоомендаций</option><option value=\"odin\">1</option><option value=\"dva\">2</option><option value=\"tri\">3</option><option value=\"chetyre\">4</option><option value=\"pyt\">5</option><option value=\"shest\">6</option><option value=\"sem\">7</option><option value=\"vosem\">8</option><option value=\"devat\">9</option><option value=\"desat\">10</option><<option value=\"eleven\">Более 10-ти</option></select></div>";
+            q_main_content.innerHTML = "<div class=\"q3\"><div class=\"q3_street\">Выберите количество уличных камер   <select id=\"question3\"><option value=\"choice\">-Выбрать-</option><option value=\"nol\">Не знаю, жду рекоомендаций</option><option value=\"odin\">1</option><option value=\"dva\">2</option><option value=\"tri\">3</option><option value=\"chetyre\">4</option><option value=\"pyt\">5</option><option value=\"shest\">6</option><option value=\"sem\">7</option><option value=\"vosem\">8</option><option value=\"devat\">9</option><option value=\"desat\">10</option><<option value=\"eleven\">Более 10-ти</option></select></div><div class=\"q3_home\">Выберите количество внутренних   <select id=\"question3\"><option value=\"choice\">-Выбрать-</option><option value=\"nol\">Не знаю, жду рекоомендаций</option><option value=\"odin\">1</option><option value=\"dva\">2</option><option value=\"tri\">3</option><option value=\"chetyre\">4</option><option value=\"pyt\">5</option><option value=\"shest\">6</option><option value=\"sem\">7</option><option value=\"vosem\">8</option><option value=\"devat\">9</option><option value=\"desat\">10</option><<option value=\"eleven\">Более 10-ти</option></select></div></div>";
             q_button.innerHTML = "<div class=\"button-q4\"><button onclick=\"next('question2','back'); \" class=\"input-btn\">Назад</button>   <button onclick=\"next('question4','forward'); \" class=\"input-btn\">Далее</button></div>";
             q_additional_content.innerHTML = "В третью очередь важен тип объекта, поэтому для начала выберите куда нужно уствновить систему. Это повысит точность расчета."
             break;
@@ -99,11 +99,11 @@ function  q_main_content_generator() {
     var content_n3;
     var content_n4;
     var content_n5 = "</div>";
-    var content_n6 = "<div class=\"row4\"><label><div class=\"textRow4\"> <input type=\"radio\" class=\"option-input radio\" name=\"example\"  value=\"yourVariant\"/>Ваш вариант</div><div class=\"inputRow4\"><input type=\"text\" id=\"inputText\"></div></label></div>";
+    var content_n6 = "<label><div class=\"row4\"><div class=\"textRow4\"> <input type=\"radio\" class=\"option-input radio\" name=\"example\"  value=\"yourVariant\"/>  Ваш вариант</div><div class=\"inputRow4\"><input type=\"text\" id=\"inputText\"></div></div></label>";
     var content = "";
     for (var i = 0; i <= 7; i++) {
          content_n2 = "<div class=\"radio1\"> <label> <div class=\"img-radio1\"><img ";
-         content_n3 = "src =" + image_q1[i] + "alt=\"\"></div><div class=\"text-radio1\"> <input type = \"radio\" class=\"option-input radio\" id = \"q1\" value =\"" + value_n1[i] + "\" >";
+         content_n3 = "src =" + image_q1[i] + "alt=\"\"></div><div class=\"text-radio1\"> <input type = \"radio\" class=\"option-input radio\" id = \"q1\" name=\"example\"  value =\"" + value_n1[i] + "\" >  ";
          content_n4 = text_n1[i] + "</div ></label> </div>";
          content += content_n2 + content_n3 + content_n4;    
     }
